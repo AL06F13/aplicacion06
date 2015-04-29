d4186f70e20756744536ccf88e9c2ae46bf33783
AL06F13 <biper.rc@gmail.com>
2015-04-29 14:34:37 -0500

    seis
    aplicacion06

---
+++ js/acciones.js
@@ -0,0 +1,17 @@
+// JavaScript Document
+$(document).ready(function(e) {
+document.addEventListener("deviceready",function(){
+	$('#btnvibrar').on('tap',function (){
+		navigator.vibrate(1000);
+	));//tap a btnvibrar
+	$('#btnbeep').on('tap', funtion()(
+	navigator.beep(1);
+	));//tap a btnbeep
+	$('#izquierda').on('swipeleft',
+	 funtion()(
+	 alert("Barrio a la izq");
+	));
+	
+}); 
+});
+
